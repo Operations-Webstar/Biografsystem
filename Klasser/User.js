7ef2315924138c239546a529a8411128f19325a6
Thumas12 <thch19ab@student.cbs.dk>
2019-10-09 14:31:33 +0200

    lavet user

---
+++ Klasser/User.js
@@ -0,0 +1,15 @@
+class user {
+    constructor(username, password, name, tlfNumber, dateOfBirth, userID) {
+        this.username = username
+        this.password = password
+        this.name = name
+        this.tlfNumber = tlfNumber
+        this.dateOfBirth = age
+        this.userID = number
+    }
+    function bookSeat(){}
+    function cancelSeat(){}
+    function logIn(){}
+    function logOut(){}
+    function checkMovies(){}
+}
