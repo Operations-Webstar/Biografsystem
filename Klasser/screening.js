8798f3c53c147535cfd0d4181f5420547167172c
rawsas1 <sas.ftw@hotmail.com>
2019-10-09 14:44:09 +0200

    Daniels klassens

---
+++ Klasser/screening.js
@@ -0,0 +1,11 @@
+// her laves klassen og de forskellige attributer tilføjes med constructor metoden.
+class Screening {
+    constructor(Time, Id, Price) {
+        this.Time = Time;
+        this.Id = Id;
+        this.Price = Price;
+    }
+    // her tilføjer jeg metoderne til klassen
+    function = hasCinemaHall(){};
+    function = hasMovie(){};
+}
