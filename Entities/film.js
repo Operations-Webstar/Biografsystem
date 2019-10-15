d64d7e669c9c3a573734352eb885126d3817527a
malenehellesen <mahe19ab@student.cbs.dk>
2019-10-15 13:26:33 +0200

    Seats checkboxes og films

--- Entities/film.js
+++ Entities/film.js
@@ -5,13 +5,18 @@ class film {
         this.genre = genre;
         this.threeD = threeD;
         this.ageRestriction = ageRestriction;
-    }
-    ageCheck(){
+}
+ageCheck(){
         var today=
         var age = document.getElementById();
     }
 }
+//forskellige film bliver defineret:
 
-let Eventyr = new Film(`felix`, '1', 'kids', 'false', '8'){
+var joker = new film ("Joker", 2, "krimi, thriller",false, 15)
 
-}
+var onceInHollywood = new film ("Once Upon a Time... in Hollywood", 3, "komedie, drama", false, 15)
+
+var  it2 = new film ( "IT 2", 4, "gyser", false, 15)
+
+var avengers = new film ( "Avengers Endgame", 5,"action, adventure", true, 11)
