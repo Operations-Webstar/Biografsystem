926dd0f8af0ae489e7e628b1aebb282d640def17
rawsas1 <sas.ftw@hotmail.com>
2019-10-15 09:44:40 +0200

    Test 2

--- Entities/film.js
+++ Entities/film.js
@@ -5,9 +5,16 @@ class film {
         this.genre = genre;
         this.threeD = threeD;
         this.ageRestriction = ageRestriction;
-}
-ageCheck(){
+    }
+    ageCheck(){
         var today=
         var age = document.getElementById();
     }
+}
+
+let Eventyr = new Film(`Eventyr`){
+    filmName = Felix På Eventyr;
+    filmId = Eventyr
+    genre = Børnefilm
+    threeD =
 }
