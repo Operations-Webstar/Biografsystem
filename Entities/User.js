c622f88f92a0eec980185ca7870566832cfc807b
rasmusraarup <rara14ad@student.cbs.dk>
2019-10-18 11:45:33 +0200

    opret bruger fødselsdag virker (næsten nu)

--- Entities/User.js
+++ Entities/User.js
@@ -1,4 +1,5 @@
 //det her er klassen, som vi skal bruge i vores user, med de tilsvarende funktioner
+/*eslint-env browser*/
 class user {
     constructor(firstName, lastName, tlfNumber, dateOfBirth, password) {
         this.firstName = firstName;
@@ -16,36 +17,50 @@ class user {
  function createUser() {
      let form_valid =true;
      let validation_message ="";
-     console.log("hejsa");
+
      let firstName = document.getElementById('firstName').value;
      let lastName = document.getElementById('lastName').value;
-     let Phone = document.getElementById('Phone').value;
-     let Birthday = document.getElementById('bday').value;
+     let phone = document.getElementById('phoneNumber').value;
+     let birthday = new Date(document.getElementById('bday').value);
      let password = document.getElementById('password').value;
 
-     if (name == null || name=="") {
-         validation_message += "Username must be filled in! \n";
+     if (firstName == null || firstName=="") {
+         validation_message += "First name must be filled in! \n";
          form_valid = false;
      }
+
+     if (lastName == null || lastName=="") {
+         validation_message += "Last name must be filled in! \n";
+         form_valid = false;
+     }
+
+
 
      if (phone == ""){
          validation_message += "Please enter a phone number \n"
          form_valid = false;
      } else if (isNaN(phone) ) {
          validation_message += "Phone numbers can only contain numbers! \n"
-         form_valid = false;
+         form_valid = false
      } else if (phone.length!=8){
          validation_message += "Phone numbers can only have a length of 8 \n"
          form_valid = false;
      }
 
 
-     if(fullDate instanceof Date && !isNaN(fullDate.valueOf()) ) {
+     console.log(birthday);
+     console.log(birthday==new Date());
+     if(birthday == 'Invalid Date') {
 
          validation_message += "insert a real date\n"
          form_valid = false;
      }
+     debugger
 
+     if ((password == null || password=="") && password.length < 3) {
+         validation_message += "your password must be longer than 3 characters \n";
+         form_valid = false;
+     }
 
      if (form_valid == false) {
          alert(validation_message);
