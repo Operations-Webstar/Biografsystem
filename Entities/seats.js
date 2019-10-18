3dd6dd4a36f115efe2266e9aa73fefc021f7e424
malenehellesen <mahe19ab@student.cbs.dk>
2019-10-18 13:16:47 +0200

    Alert checkboxes - gå til kurv

--- Entities/seats.js
+++ Entities/seats.js
@@ -1,9 +1,53 @@
 // Her laves klassen seats med de korrekte attributer og metoder
-class seats {
-    constructor(Id, hallId) {
-        this.Id = Id;
-        this.hallId = hallId;
+class Seat {
+    constructor(seat, row) {
+        this.seat = seat;
+        this.row = row;
     }
-    findSeat(){}
-    isAvailable(){}
 }
+
+var reserveSeatsButton = document.getElementById("reserveSeats")
+var seatCheckbox = document.getElementsByClassName("Seat");
+console.log(seatCheckbox)
+console.log(seatCheckbox.length)
+
+reserveSeatsButton.onclick = function(){
+
+    for(i=0; i < seatCheckbox.length; i++){
+        if(seatCheckbox[i].checked == true){
+            document.location.href = 'Kurv.html'
+            alert(seatCheckbox[i].id)
+           // document.location.href = 'Kurv.html'
+        }
+    }
+
+}
+
+
+/*
+var i =0;
+var len = seatCheckbox.length;
+console.log(len)
+var seatChosen = "";
+var validSeat = false;
+
+function validSeats() {
+    for (i=0; i<len; i++) {
+        if(seatCheckbox[i].checked) {
+                    console.log(seatCheckbox[i].id)
+            /*
+            seatChosen = seatCheckbox[i].value;
+            validSeat = true;
+            document.location.href = 'Kurv.html';
+        }
+    }
+}*/
+
+
+
+
+
+
+
+
+
