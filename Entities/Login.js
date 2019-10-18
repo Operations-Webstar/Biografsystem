328037ef31f70a8e285b426cb23867ae01509661
Thumas12 <thch19ab@student.cbs.dk>
2019-10-18 12:34:33 +0200

    Commit til Daniel

--- Entities/Login.js
+++ Entities/Login.js
@@ -1,5 +1,5 @@
 
-checkLogin()
+function checkLogin()
 {
     let storedName = localStorage.getItem('firstName');
     let storedPassword = localStorage.getItem('password');
@@ -7,9 +7,15 @@ checkLogin()
     let enteredName = document.getElementById('enteredFirstName');
     let enteredPassword = document.getElementById('enteredPassword');
 
-    if (enteredName.value === storedName && enteredPassword === storedPassword) {
-        alert('Logged in')
+    if (enteredName.value === storedName && enteredPassword.value === storedPassword) {
+        localStorage.setItem('logInStatus', 'true')
+        window.location.href (index.html)
     } else {
         alert('wrong pass or user')
     }
 }
+
+function signOut() {
+    localStorage.setItem('logInStatus', 'false')
+}
+
