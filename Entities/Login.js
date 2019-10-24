8a2f92913bdda2433bb421fe5d5a0954c7a27414
Thumas12 <thch19ab@student.cbs.dk>
2019-10-24 12:34:07 +0200

    Fikset lidt godt

--- Entities/Login.js
+++ Entities/Login.js
@@ -3,13 +3,13 @@ function checkLogin()
 {
     let enteredName = document.getElementById('enteredFirstName').value.toString();
     let enteredPassword = document.getElementById('enteredPassword').value;
-    let storedUsers = JSON.parse(localStorage.getItem('users'))
+    let storedUsers = JSON.parse(localStorage.getItem('users'));
     for(let i=0; i<storedUsers.length;i++){
-        if (enteredName == storedUsers[i].tlfNumber && enteredPassword == storedUsers[i].password) {
+        if (enteredName == storedUsers[i].tlfNumber && enteredPassword == storedUsers[i].password){
             localStorage.setItem('logInStatus', 'true');
             window.location.href = 'index.html';
         }
-            else {
+         else {
             alert('wrong pass or user');
         }
     }
