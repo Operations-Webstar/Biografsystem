ba77fb372ac86fdf5073cb3bd16adc0b71ee8702
malenehellesen <mahe19ab@student.cbs.dk>
2019-11-05 10:18:39 +0100

    Kommentarer seats

--- Entities/seats.js
+++ Entities/seats.js
@@ -6,12 +6,13 @@ class Seat {
     }
 }
 // Variablen reserveSeatsButton returnerer den værdi der er koblet på Id'et
+
 var reserveSeatsButton = document.getElementById("reserveSeats")
 var seatCheckbox = document.getElementsByClassName("Seat");
 
 reserveSeatsButton.onclick = function(){
 
-// Her tjekker den om der er er nogen af checkboksene er vinget af
+// Her tjekker den om der er er nogen af checkboksene som er vinget af startende fra checkboks 0
 
             var counter = 0
             var finalMessage = "Du har nu reserveret: "
@@ -22,13 +23,15 @@ reserveSeatsButton.onclick = function(){
                     counter++
         }
     }
-// Er en eller flere checkbokse vinget af, vil følgende alert poppe op.
+// Er en eller flere checkbokse vinget af, vil følgende alert poppe op:
+
             if (counter>0) {
                 alert(finalMessage)
                 document.location.href = 'Mine_bookninger.html'
 
             }
-// Er ingen af checkboksene checked, vil følgende alert poppe op.
+// Er ingen af checkboksene checked, vil følgende alert poppe op:
+
             if (counter==0) {
                 alert("Hov! Du mangler at markere de ønskede sæder.")
             }
