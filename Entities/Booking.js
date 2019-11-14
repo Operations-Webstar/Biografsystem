e1a9a2fe11d2f8967235d5c122817b67f2463a8a
rawsas1 <sas.ftw@hotmail.com>
2019-11-14 13:09:38 +0100

    ½

--- Entities/Booking.js
+++ Entities/Booking.js
@@ -6,8 +6,12 @@ class Booking {
     }
     // function updateSeatsAvailable(){}
 }
+//d
+var displayFilms = localStorage.getItem("film")
+document.getElementById('filmNavn').innerHTML = displayFilms;
 //Malene
 var displaySeats = JSON.parse(localStorage.getItem("seatsChosen"))
 console.log(displaySeats)
 debugger
 document.getElementById('seats123').innerHTML = displaySeats;
+
