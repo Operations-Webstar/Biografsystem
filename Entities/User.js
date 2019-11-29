ddbf0e27ab9352ef53a1450dd43a397696a52a95
rara14ad@student.cbs.dk <rara14ad@student.cbs.dk>
2019-11-29 11:55:13 +0100

    test if push works

--- Entities/User.js
+++ Entities/User.js
@@ -35,73 +35,14 @@ class User {
 
 
 
-
-
     //function bookSeat(){};
     //function cancelSeat(){};
     //function logIn(){};
     //function logOut(){};
     //function checkMovies(){};
 
-    //en static funktion der laver en instans af klassen User, som man så bruger JSON, til at lagre i localStorage
-    //Bruger forskellige if statements, til at sørge for den indtastede info er korrekt
-    static createUser() {
-        let form_valid = true;
-        let validation_message = "";
-        let storedUsers = Tools.getAllUsers();
-        this.firstName = document.getElementById('firstName').value;
-        this.lastName = document.getElementById('lastName').value;
-        this.tlfNumber = document.getElementById('phoneNumber').value;
-        this.dateOfBirth = new Date(document.getElementById('bday').value);
-        this.password = document.getElementById('password').value;
 
 
-//tjekker om firstname har en værdi eller er tom, hvis ja, så bliver form_valid = false, Rasmus
-        if (this.firstName == null || this.firstName === "") {
-            validation_message += "First name must be filled in! \n";
-            form_valid = false;
-        }
-//tjekker om lastName har en værdi eller er tom, hvis ja, så bliver form_valid = false, Rasmus
-        if (this.lastName == null || this.lastName === "") {
-            validation_message += "Last name must be filled in! \n";
-            form_valid = false;
-        }
-
-//tjekker om phone er tom, om det ikke er et nummmer og om længden ikke er 8, hvis ja, så bliver form_valid = false, Rasmus
-        if (this.tlfNumber === "") {
-            validation_message += "Please enter a phone number \n";
-            form_valid = false;
-        } else if (isNaN(this.tlfNumber)) {
-            validation_message += "Phone numbers can only contain numbers! \n";
-            form_valid = false
-        } else if (this.tlfNumber.length !== 8) {
-            validation_message += "Phone numbers can only have a length of 8 \n";
-            form_valid = false;
-        }
-
-        //tjekker om birthday kommer ud som invalid date, hvis den gør, så bliver form_valid = false, Rasmus
-        if (this.dateOfBirth === 'Invalid Date') {
-            validation_message += "insert a real date\n";
-            form_valid = false;
-        }
-
-        //tjekker om password er tom, lig null eller ikke længere end 3 karakter, hvis ja, så bliver form_valid = false, Rasmus
-        if ((this.password == null || this.password === "") && this.password.length < 3) {
-            validation_message += "your password must be longer than 3 characters \n";
-            form_valid = false;
-        }
-
-        //hvis form_valid == true dvs. at alle krav til sign in funktionen er blevet udfyldt, så sætter man storedUsers
-        // arrayet, nu må den nye bruger ind i localstorage via stringify.
-        // ellers så alerter den (validation_message) med tilhørende strings. Rasmus/Thomas
-        if (form_valid === true) {
-            storedUsers.push(new User(this.firstName, this.lastName, this.tlfNumber, this.dateOfBirth, this.password));
-            localStorage.setItem('users', JSON.stringify(storedUsers));
-            window.location.href = 'Login.html'
-        } else {
-            alert(validation_message);
-        }
-    };
     //Thomas
     //en static funktion der logger ind bruger ind, dette gøres ved at kalde den statisk. Den tjekker om telefonnummer og
     // password passer sammen. Hvis Ja, så sætter den localStorage keyen 'activeUser' til at være den User
@@ -150,15 +91,7 @@ class Admin extends User {
     this._adminRights = x;
     }
 
-    //en metode der gør at admin, kan se hvilket telefon nummer, der hører til hvilket navn.
-    showUser(){
-        let target = Tools.getUser();
-        if(target === undefined){
-            alert('User do not exist')
-        }else {
-      document.getElementById('navn').innerHTML = target._firstName + ' ' + target._lastName;
-           document.getElementById('number').innerHTML = target._tlfNumber}
-    }
+
 
     //En metode som fjerner en user fra det gemte array, som ligger i localstorage
     deleteUser(){
