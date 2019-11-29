52b393bfd856465fa2146e83d3a32f60e2577232
rara14ad@student.cbs.dk <rara14ad@student.cbs.dk>
2019-11-29 12:05:57 +0100

    added in age check

--- Entities/film.js
+++ Entities/film.js
@@ -16,30 +16,28 @@ class Film {
 
             let today = new Date();
             let dateOfBirth = Birthday;
-            console.log(typeof(dateOfBirth));
+            var pattern = /(\d{2})\-(\d{2})\-(\d{4})/;
+            console.log(dateOfBirth.replace(pattern,'$3-$2-$1'))
+            var dt = new Date(dateOfBirth.replace(pattern,'$3-$2-$1'));
+            console.log(dt)
 
             //Bruger new date, så at user.dateOfbirth kommer i Dato format, så vi kan bruge de næste funktioner
 
-            if (typeof dateOfBirth === "undefined") {
-                console.log('no date of birth');
-            } else {
-                var birthToDate = new Date(dateOfBirth)
-                console.log(birthToDate);
-            }
+
             //sætter age i lige med idag årstal minus birthDate årstal og trækker dem fra hinanden
 
-            let age = today.getFullYear() - birthToDate.getFullYear();
-            console.log(age);
+            let age = today.getFullYear() - dt.getFullYear();
+
 
             //sætter m lig med nuværende måned - fødselsdags måned
 
-            let m = today.getMonth() - birthToDate.getMonth();
+            let m = today.getMonth() - dt.getMonth();
 
             //Hvis 0 er større end M, har personen allerede haft fødselsdag, så behøves der ikke trækkes 1 fra.
             //Dog hvis m === 0 eller at idag's dag ikke er større end fødselsdags dagen, så minusser man age med 1
             // da personen ikke har nået at have haft fødselsdag
 
-            if (m < 0 || (m === 0 && today.getDate() < birthToDate.getDate())) {
+            if (m < 0 || (m === 0 && today.getDate() < dt.getDate())) {
                 age - 1
             }
             return age
