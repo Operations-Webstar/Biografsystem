638c9ecc5815f38d2dd81f1007ed7dd8ac7ca1ad
Thumas12 <thch19ab@student.cbs.dk>
2020-05-05 19:49:36 +0200

    Lavet delete funktion

--- pageJavascript/mine_bookninger.js
+++ pageJavascript/mine_bookninger.js
@@ -3,6 +3,7 @@
 //Der konstrueres en tabel med tre rows, hvori brugerens bookings vil blive displayet.
 function buildTable(data) {
     let table = document.createElement("table");
+    table.id = 'table'
     let fields = Object.keys(data[0]);
     let headRow = document.createElement("tr");
     fields.forEach(function(field) {
@@ -12,7 +13,19 @@ function buildTable(data) {
     });
     table.appendChild(headRow);
 
-    data.forEach(function(object){
+    data.forEach(function(object, index){
+        let button = document.createElement('button')
+        button.innerHTML = 'Slet booking'
+        button.type = 'button'
+        //sætter en event listener på, som så sletter den booking, den er ved.
+        button.addEventListener("click", () =>{
+            axios.delete('http://localhost:3000/bookings/' + JSON.parse(sessionStorage.getItem('info'))[index].booking_id)
+                .then(result => {
+                    console.log(result)
+                }).catch(err => {
+                console.log(err)
+            })
+        });
         let row = document.createElement("tr");
         fields.forEach(function(field){
             let cell = document.createElement("td");
@@ -23,6 +36,8 @@ function buildTable(data) {
             row.appendChild(cell);
         });
         table.appendChild(row);
+        table.appendChild(button)
+
     });
     return table;
 }
@@ -40,7 +55,8 @@ function myBooking () {
                             Film: result1.data.showing.film.filmName+ ".",
                             Tid: result1.data.showing.dateTime + ".",
                             Sal: result1.data.showing.hall.hallName + ".",
-                            Sæder:allInfo.seats
+                            Sæder:allInfo.seats,
+                            booking_id: allInfo._id
                         })
                         // Informationen om bookingerne indsættes i en "info"-key der oprettes i session storage.
                         sessionStorage.setItem("info", JSON.stringify(x))
@@ -51,14 +67,22 @@ function myBooking () {
 // Hvis keyen "info" i session storage er tom, alertes der at der ikke er nogen bookinger
 function f() {
     let info = JSON.parse(sessionStorage.getItem('info'))
-        console.log(info)
     if(info == null){
         alert('du har ingen bookinger')
         return
     }
+    let bookingInfo = info.map(doc => {
+        return {
+            Film: doc.Film,
+            Tid: doc.Tid,
+            Sal: doc.Sal,
+            Sæder: doc.Sæder
+        }
+    })
     // Er der foretaget bookinger vises de i tabellen der konstrueres under buildTable
     document.getElementById('bookingButton').style.display = 'none'
-        document.querySelector("#displayOfMovies").appendChild(buildTable(info));
+        document.querySelector("#displayOfMovies").appendChild(buildTable(bookingInfo))
+
 }
 
 window.onload= myBooking()
